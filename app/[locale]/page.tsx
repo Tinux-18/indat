ba973e4ba1950488f8ffdfe2ab58516d2132081d
Constantin Rigu <constantin@indat.tech>
2025-01-23 16:42:55 +0100

    Fix warnings

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,9 +2,10 @@ import { Metadata } from "next"
 import { Footer } from "components/Home/Footer"
 import { Header } from "components/Home/Header"
 import { Video } from "components/Home/Video"
+import { Studies } from "components/Home/Studies"
 
 export const metadata: Metadata = {
-  title: "Welcome to InDat",
+  title: "InDat",
   twitter: {
     card: "summary_large_image",
   },
@@ -13,9 +14,10 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <main className=" cursor-default select-none bg-white dark:bg-gray-900">
+    <main className="ml-2 cursor-default select-none bg-white dark:bg-gray-900">
       <Header></Header>
       <Video></Video>
+      <Studies></Studies>
       <Footer></Footer>
     </main>
   )
